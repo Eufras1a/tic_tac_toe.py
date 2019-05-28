8b7c85a7faaa0139e3848d180f2cf25e1d98aaa1
Eufrasia <prashantshaan98@gmail.com>
2019-05-28 22:24:36 +0530

    Finished the win checker

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,62 +1,88 @@
-from pprint import pprint
 from random import randint, choice
 
 board = {}
 list_board = []
-sym = {}
-win = False
+sym = {}  # symbol player/controller: tick or cross
+
+
 def main():
     global sym
-    sym['comp'] = controller()
+    sym['comp'] = controller()  # assigning computer AI symbol
     set_board()
     play()
 
+
 # main code of the game
 def play():
-    global win
-    start = initial_set() #  if comp == 1, then comp goes first
+    start = initial_set()  # if comp == 1, then comp goes first
     display_board()
-    print(start)
+
     if start == 1: 
         print('Computer starts first!!')
-    else:
-        print('You go first!!!')
+        comp_turn()
 
-    while not win:
-        if start == 0: 
-            player_turn()
-            start = 1
-        else:
-            comp_turn()
-            start = 0
-
+    while True:
+        player_turn()
         display_board()
-        win = win_checker()
+        if win_checker(sym['player']):
+            break
+        
+        comp_turn()
+        if win_checker(sym['comp']):
+            break
 
 
 # check if anyone has won ? 
-def win_checker():
-    global list_board, win
-    s = []
+def win_checker(temp):
+    global list_board, board
 
-    #check for if anyone makes a line
+    # check for if anyone makes a line
+    if board['top-L'] == board['top-R'] == board['top-M'] == temp :
+        return end_msg(temp)
+    elif board['mid-L'] == board['mid-R'] == board['mid-M'] == temp :
+        return end_msg(temp)
+    elif board['bottom-L'] == board['bottom-R'] == board['bottom-M'] == temp :
+        return end_msg(temp)
+    elif board['top-L'] == board['bottom-L'] == board['mid-L'] == temp :
+        return end_msg(temp)
+    elif board['top-R'] == board['bottom-R'] == board['mid-R'] == temp :
+        return end_msg(temp)
+    elif board['top-M'] == board['bottom-M'] == board['mid-M'] == temp :
+        return end_msg(temp)
+    elif board['top-L'] == board['mid-M'] == board['bottom-R'] == temp :
+        return end_msg(temp)
+    elif board['top-R'] == board['mid-M'] == board['bottom-L'] == temp :
+        return end_msg(temp)
 
-    #check for tie
-    if list_board == s:
-        print("tie")
-        return True
+    # if the board is filled then tie
+    if not list_board:
+        return end_msg('tie')
+
+    return False
+
+
+
+def end_msg(temp):
+    if temp == sym['player']:
+        print('Congratulations you won!!!')
+    elif temp == sym['player']:
+        print('Game Over')
     else:
-        return False
-
+        print()
+        print("It's a tie!!!")
+    
+    return True
 
 
 # computer's move
 def comp_turn():
     global sym, board, list_board
+    print("It's computer's turn.")
     move = choice(list_board)
     board[move] = sym['comp']
     list_board.remove(move)
-    
+    #sleep(2)
+    display_board()
 
 # ask user for their move 
 def player_turn():
@@ -65,16 +91,14 @@ def player_turn():
     while True:
         temp = input()
         if board.get(temp, 'Fail') == 'Fail':
-            print('error: invalid answer, try again')
+            print('error: invalid keyword, try again')
         elif board[temp] != " ":
-            print('try again')
-        else: 
+            print('that place is already filled duh!!!')
+        else:
             board[temp] = sym['player']
             list_board.remove(temp)
             break
-
-
-
+    display_board()
 
 # display the current state of the game baord
 def display_board():
@@ -84,15 +108,17 @@ def display_board():
     print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-----') 
     print(board['bottom-L'] + '|' + board['bottom-M'] + '|' + board['bottom-R']) 
+    print()
+    print()
 
 # decide who goes first
 def initial_set():
     return randint(0,1)
 
+
 # decide what symbol does the player want to use 
 def controller():
     global sym
-
     sym = { 'player': ' ', 'comp': ' '}
     print('Would you like to play with "X" or "O" ?')
 
@@ -100,15 +126,14 @@ def controller():
         sym['player'] = input().upper()
         if sym['player'] == 'X':
             return 'O'
-            
+        
         elif sym['player'] == 'O':
             return 'X'
              
         else:
-            print('pls either enter alphabetical X or O to proced further into the game')
+            print('pls either enter alphabetical X or O to proced further into the game', end = ': ')
 
         
-
 # set the baord to inital state
 def set_board():
     global board, list_board
@@ -125,4 +150,8 @@ def set_board():
     list_board = list(board.keys())
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        print('Would you like to play again ?', 'Press "y" to play again or to quit press any key', sep = '\n')
+        if input().lower() != 'y':
+            break
